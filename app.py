ea64356ea8a5232bb44ce993acd2524cf162b928
Julio Cesar <81919217+Julio-JPereira@users.noreply.github.com>
2023-02-21 16:23:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,139 @@
+import dash
+from dash import Dash, html, dcc, Input, Output
+#import dash
+import plotly.express as px
+import pandas as pd
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.MATERIA])
+
+
+#-----------------------------------------------------------------------------------------------------------------
+grid = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(), md=4),
+                dbc.Col(dcc.Graph(),md=8)
+            ]
+        )
+    ]
+)
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Pág 1", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("Outros", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="Dashboard de analise preditiva",
+    brand_href="#",
+    color="#27293d",
+    dark=True,
+)
+
+navbar2 = dbc.NavbarSimple(
+    #children=[
+        #dbc.NavItem(dbc.NavLink("Pág 1", href="#")),
+     #   dbc.DropdownMenu(
+      #      children=[
+                #dbc.DropdownMenuItem("Outros", header=True),
+                #dbc.DropdownMenuItem("Page 2", href="#"),
+                #dbc.DropdownMenuItem("Page 3", href="#"),
+       #     ],
+        #    nav=True,
+        #    in_navbar=True,
+            #label="More",
+        #),
+    #],
+    brand="© 2023 Copyright: Centro Universitário FEI",
+    brand_href="#",
+    color="#27293d",
+    dark=True,
+)
+#-----------------------------------------------------------------------------------------------
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_excel("Vendas.xlsx")
+df2 = pd.read_excel("Falhas.xlsx") #parse_dates=['Falhas'])
+#print(df2['Falhas'])
+df3 = pd.read_excel("MesxMes.xlsx")
+df4 = pd.read_excel("Leitura.xlsx")
+
+
+
+random_x = [100, 2000, 550]
+names = ['A', 'B', 'C']
+
+colors = {
+    'background': '#27293d',
+    'text': 'pink'
+}
+
+colors2 = {
+    'background-image': 'linear-gradient(to right, #ab63fa, #00b0f6)',
+
+}
+
+#color3='rgb({}, {}, {})'.format((i/100*255),(i/100*255),(i/100*255)),
+#Criando o gráfico produto = sensores, quantidade = n° de falhas                      color = De acordo com o sensor
+#fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group", color_continuous_scale=px.colors.qualitative.Pastel2)#template='plotly_dark'
+
+
+opcoes = list(df['ID Loja'].unique())
+opcoes.append("Todas as Lojas")
+app.title = 'Manutenção preditiva'
+#app.config = colors.update(
+
+#)
+
+app_color = {"graph_bg": "#082255", "graph_line": "#007ACE", 'background-color': '#007ACE'}
+plot_layout = {
+    "paper_bgcolor": "#141414",
+}
+
+
+
+
+
+#final = df2.tail(1),
+
+#str = (final.to_string()),
+#print('String:', str)
+#destro = ','.join(df2.tail(1))
+#print(destro)
+
+
+
+#app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.MATERIA])
+
+app.layout = html.Div(
+    [
+        navbar,
+        # main app framework
+
+        html.Div([
+            dcc.Link(page['name']+"  |  ", href=page['path'])
+            for page in dash.page_registry.values()
+        ]),
+        html.Hr(),
+
+        # content of each page
+        dash.page_container,
+        navbar,
+        navbar2,
+    ], style={'background-color': '#1e1e2f', 'margin': '0', 'padding': '0', "content": "width=device-width, initial-scale=1"}
+)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pg2.py
@@ -0,0 +1,46 @@
+import dash
+from dash import Dash, html, dcc, Input, Output, dash_table
+#import dash
+import plotly.express as px
+import pandas as pd
+#import dash_bootstrap_components as dbc
+#import dash_table
+#from dash_table.Format import Format, Group, Scheme, Symbol
+import plotly.graph_objects as go
+#import plotly.offline as pyo
+
+dash.register_page(__name__)
+
+df = px.data.tips()
+df2 = pd.read_excel("Vendas.xlsx")
+
+color1 = '#d3d3d3'
+color2 = 'white'
+
+
+fig9 = go.Figure(data=[go.Table(
+    header=dict(values=list(df2.columns), fill_color=[[color1]]), # dados do cabeçalho
+    cells=dict(values=[df2['Valor Final'], df2['Valor Unitário'], df2['Quantidade'], df2['Produto'], df2['ID Loja'], df2['Data'], df2['Código Venda']], fill_color=[[color2]], line_color=[['pink']])), # dados do corpo da tabela
+
+]
+)
+#pyo.offline.plot(fig9, filename = "tabela-01.html")
+
+layout = html.Div(
+    [
+        dcc.RadioItems([x for x in df.day.unique()], id='day-choice'),
+        dcc.Graph(id='bar-fig',
+                  figure=px.bar(df, x='smoker', y='total_bill')),
+        html.Div(
+        [dash_table.DataTable(
+        id='table',
+        data=df2.to_dict('records'),
+        style_cell=dict(textAlign='center'),
+        style_header=dict(backgroundColor="#001d38"),
+        style_data=dict(backgroundColor="#27293d"),
+        style_table={'height': '390px', 'overflowY': 'auto'}, #auto
+        fixed_rows={'headers': True},
+        style_as_list_view=True,
+    )], style={'display': 'inline-block', 'width': '50%', 'height': '400px', 'background-color': '#1e1e2f', 'margin-left': '25%', 'color': 'pink'}),
+    ],
+)
