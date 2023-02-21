a3b556e7099c349885590c2bd5300e33d2cba68d
Julio Cesar <81919217+Julio-JPereira@users.noreply.github.com>
2023-02-21 16:25:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.MATERIA])
-
+server = app.server
 
 #-----------------------------------------------------------------------------------------------------------------
 grid = html.Div(
