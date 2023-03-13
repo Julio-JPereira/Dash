7520da1a19a0e7b00a11e5da6cae17931cd8a649
Julio Cesar <81919217+Julio-JPereira@users.noreply.github.com>
2023-03-13 01:43:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.MATERIA])
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.MATERIA],  meta_tags=[{'name': 'viewport', 'content': 'width-device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 server = app.server
 
 #-----------------------------------------------------------------------------------------------------------------
